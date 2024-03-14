dfc1489de5fb83d6ed896e04caab1d4770eb681c
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-03-14 10:15:23 -0400

    Move to own merger dir

--- src/merger/known.rs
+++ src/merger/known.rs
@@ -1,54 +1,8 @@
+use super::core::{Merger, Padding, Point};
 use crate::{cell::ImageCell, core::Image, functions::paste};
 use image::Pixel;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use std::{marker::Sync, ops::DerefMut};
-
-/// Represents a point on any canvas.
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct Point {
-    pub x: u32,
-    pub y: u32,
-}
-
-/// Represents the padding between images on a canvas.
-/// # Fields
-/// * `x` - The padding between images on the x axis.
-/// * `y` - The padding between images on the y axis.
-pub type Padding = Point;
-
-/// The Merger trait that all mergers must implement. This trait allows the merger to paste images to a canvas.
-/// # Type Parameters
-/// * `P` - The pixel type of the underlying image.
-pub trait Merger<P>
-where
-    P: Pixel + Sync,
-    <P as Pixel>::Subpixel: Sync,
-{
-    /// Returns a reference to the underlying canvas.
-    fn get_canvas(&self) -> &Image<P, image::ImageBuffer<P, Vec<P::Subpixel>>>;
-
-    /// Consumes the underlying merger and returns the canvas.
-    fn into_canvas(self) -> Image<P, image::ImageBuffer<P, Vec<P::Subpixel>>>;
-
-    /// Allows the merger to push an image to the canvas. This can be used in a loop to paste a large number of images without
-    /// having to hold all them in memory.
-    /// # Arguments
-    /// * `image` - The image to push onto the canvas. Its pixel type, `P`, must match the canvas, and its `Container` must be dereferenceable to
-    /// a slice of `P::Subpixel`s.
-    fn push<Container>(&mut self, image: &Image<P, image::ImageBuffer<P, Container>>)
-    where
-        Container: DerefMut<Target = [P::Subpixel]>;
-
-    /// Allows the merger to bulk push N images to the canvas. This is useful for when you have a large number of images to paste.
-    /// The downside is that you have to hold all of the images in memory at once, which can be a problem if you have a large number of images.
-    /// # Arguments
-    /// * `images` - The images to push onto the canvas. Note that the argument type is `&[&Image<...>]`, the func
-    /// does not need to take ownership of the images, it only needs to read them. The pixel type, `P`, of the images must match the canvas, and
-    /// their `Container` must be dereferenceable to a slice of `P::Subpixel`s.
-    fn bulk_push<Container>(&mut self, images: &[&Image<P, image::ImageBuffer<P, Container>>])
-    where
-        Container: DerefMut<Target = [P::Subpixel]> + Sync;
-}
 
 /// A known size merger that allows you to paste images onto a canvas. This merger is useful when you already know the size
 /// of all the images being pushed onto the canvas.
---
+++ src/merger/mod.rs
@@ -0,0 +1,5 @@
+mod core;
+mod known;
+
+pub use core::*;
+pub use known::*;
