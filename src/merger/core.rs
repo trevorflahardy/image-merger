814134b16754275fea6bfb5a97d4d8ea63364e4a
NextChai <75498301+NextChai@users.noreply.github.com>
2024-06-27 23:53:09 -0400

    Clean mergers and make all mergers more generic and have better generic handling

--- src/merger/core.rs
+++ src/merger/core.rs
@@ -1,6 +1,5 @@
-use crate::{cell::ImageCell, core::Image, functions::paste};
+use crate::core::Image;
 use image::Pixel;
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use std::{marker::Sync, ops::DerefMut};
 
 /// Represents a point on any canvas.
@@ -19,25 +18,24 @@ pub type Padding = Point;
 /// The Merger trait that all mergers must implement. This trait allows the merger to paste images to a canvas.
 /// # Type Parameters
 /// * `P` - The pixel type of the underlying image.
-pub trait Merger<P>
+pub trait Merger<P, Container>
 where
     P: Pixel + Sync,
     <P as Pixel>::Subpixel: Sync,
+    Container: DerefMut<Target = [P::Subpixel]> + Sync,
 {
     /// Returns a reference to the underlying canvas.
-    fn get_canvas(&self) -> &Image<P, image::ImageBuffer<P, Vec<P::Subpixel>>>;
+    fn get_canvas(&self) -> &Image<P, image::ImageBuffer<P, Container>>;
 
     /// Consumes the underlying merger and returns the canvas.
-    fn into_canvas(self) -> Image<P, image::ImageBuffer<P, Vec<P::Subpixel>>>;
+    fn into_canvas(self) -> Image<P, image::ImageBuffer<P, Container>>;
 
     /// Allows the merger to push an image to the canvas. This can be used in a loop to paste a large number of images without
     /// having to hold all them in memory.
     /// # Arguments
     /// * `image` - The image to push onto the canvas. Its pixel type, `P`, must match the canvas, and its `Container` must be dereferenceable to
     /// a slice of `P::Subpixel`s.
-    fn push<Container>(&mut self, image: &Image<P, image::ImageBuffer<P, Container>>)
-    where
-        Container: DerefMut<Target = [P::Subpixel]>;
+    fn push(&mut self, image: &Image<P, image::ImageBuffer<P, Container>>);
 
     /// Allows the merger to bulk push N images to the canvas. This is useful for when you have a large number of images to paste.
     /// The downside is that you have to hold all of the images in memory at once, which can be a problem if you have a large number of images.
@@ -45,158 +43,5 @@ where
     /// * `images` - The images to push onto the canvas. Note that the argument type is `&[&Image<...>]`, the func
     /// does not need to take ownership of the images, it only needs to read them. The pixel type, `P`, of the images must match the canvas, and
     /// their `Container` must be dereferenceable to a slice of `P::Subpixel`s.
-    fn bulk_push<Container>(&mut self, images: &[&Image<P, image::ImageBuffer<P, Container>>])
-    where
-        Container: DerefMut<Target = [P::Subpixel]> + Sync;
-}
-
-/// A known size merger that allows you to paste images onto a canvas. This merger is useful when you already know the size
-/// of all the images being pushed onto the canvas.
-/// # Type Parameters
-/// * `P` - The pixel type of the underlying image.
-pub struct KnownSizeMerger<P: Pixel> {
-    canvas: ImageCell<P, image::ImageBuffer<P, Vec<P::Subpixel>>>,
-    image_dimensions: (u32, u32), // The dimensions of the images being pasted (images must be a uniform size)
-    num_images: u32,              // The number of images that have been pasted to the canvas
-    images_per_row: u32,          // The number of pages per row.
-    last_pasted_index: i32, // The index of the last pasted image, starts at -1 if not images have been pasted.
-    total_rows: u32,        // The total number of rows currently on the canvas.
-    padding: Option<Padding>,
-}
-
-impl<P> KnownSizeMerger<P>
-where
-    P: Pixel + Sync,
-    <P as Pixel>::Subpixel: Sync,
-{
-    /// Constructs a new KnownSizeMerger.
-    /// # Arguments
-    /// * `image_dimensions` - The dimensions of the images being pasted (images must be a uniform size)
-    /// * `images_per_row` - The number of images per row.
-    /// * `total_images` - The total numbr of images to be in the final canvas.
-    /// * `padding` - The padding between images, or None for no padding.
-    pub fn new(
-        image_dimensions: (u32, u32),
-        images_per_row: u32,
-        total_images: u32,
-        padding: Option<Padding>,
-    ) -> Self {
-        let total_rows = (total_images + images_per_row - 1) / images_per_row;
-
-        let image_gaps_x = (images_per_row - 1) * padding.as_ref().map(|p| p.x).unwrap_or(0);
-        let image_gaps_y = (total_rows - 1) * padding.as_ref().map(|p| p.y).unwrap_or(0);
-
-        let canvas: Image<P, image::ImageBuffer<P, Vec<P::Subpixel>>> = Image::new(
-            (image_dimensions.0 * images_per_row) + image_gaps_x,
-            (image_dimensions.1 * total_rows) + image_gaps_y,
-        );
-
-        Self {
-            canvas: ImageCell::new(canvas),
-            image_dimensions,
-            num_images: 0,
-            images_per_row,
-            last_pasted_index: -1,
-            total_rows,
-            padding,
-        }
-    }
-
-    /// Returns the number of images that have been pasted to the canvas.
-    pub fn get_num_images(&self) -> u32 {
-        self.num_images
-    }
-
-    #[inline(always)]
-    fn additional_space(&self) -> u32 {
-        (self.images_per_row * self.total_rows) - self.num_images
-    }
-
-    fn get_paste_coordinates_unchecked(&self, index: u32) -> (u32, u32) {
-        let offset_x = index % self.images_per_row;
-        let offset_y = index / self.images_per_row;
-
-        let padding_x = self.padding.as_ref().map(|p| p.x).unwrap_or(0) * offset_x;
-        let padding_y = self.padding.as_ref().map(|p| p.y).unwrap_or(0) * offset_y;
-
-        let x = (offset_x * self.image_dimensions.0) + padding_x;
-        let y = (offset_y * self.image_dimensions.1) + padding_y;
-
-        (x, y)
-    }
-
-    fn get_next_paste_coordinates(&mut self) -> (u32, u32) {
-        if self.additional_space() == 0 {
-            panic!("No more space on the canvas!");
-        }
-
-        self.get_paste_coordinates_unchecked((self.last_pasted_index + 1) as u32)
-    }
-
-    /// Removes an image from the canvas at a given index. Indexing starts at 0 and works left to right, top to bottom.
-    /// # Arguments
-    /// * `index` - The index of the image to remove.
-    pub fn remove_image(&mut self, index: u32) {
-        let offset_x = index % self.images_per_row;
-        let offset_y = index / self.images_per_row;
-
-        let x = offset_x * self.image_dimensions.0;
-        let y = offset_y * self.image_dimensions.1;
-
-        let black_image: Image<P, image::ImageBuffer<P, Vec<P::Subpixel>>> =
-            Image::new(self.image_dimensions.0, self.image_dimensions.1);
-
-        paste(&self.canvas, &black_image, Point { x, y });
-    }
-}
-
-impl<P> Merger<P> for KnownSizeMerger<P>
-where
-    P: Pixel + Sync,
-    <P as Pixel>::Subpixel: Sync,
-{
-    fn get_canvas(&self) -> &Image<P, image::ImageBuffer<P, Vec<P::Subpixel>>> {
-        &self.canvas
-    }
-
-    fn into_canvas(self) -> Image<P, image::ImageBuffer<P, Vec<P::Subpixel>>> {
-        self.canvas.into_inner()
-    }
-
-    fn push<Container>(&mut self, image: &Image<P, image::ImageBuffer<P, Container>>)
-    where
-        Container: DerefMut<Target = [P::Subpixel]>,
-    {
-        let (x, y) = self.get_next_paste_coordinates();
-
-        paste(&self.canvas, image, Point { x, y });
-
-        self.last_pasted_index += 1;
-        self.num_images += 1;
-    }
-
-    fn bulk_push<Container>(&mut self, images: &[&Image<P, image::ImageBuffer<P, Container>>])
-    where
-        Container: DerefMut<Target = [P::Subpixel]> + Sync,
-    {
-        // If we can't fit all the images we need to panic.
-        if self.additional_space() < images.len() as u32 {
-            // TODO: Maybe only take as many images as we can fit?
-            panic!("There is not enough space on the canvas to fit all the requested images.");
-        }
-
-        (0..images.len()).into_par_iter().for_each(|index| {
-            let image = images[index];
-
-            // The image coordinates can easily be calculated by using the last_pasted_index
-            // and making the calculations ourselves.
-            let offset_index = (index as i32 + self.last_pasted_index + 1) as u32;
-
-            let (x, y) = self.get_paste_coordinates_unchecked(offset_index);
-            paste(&self.canvas, image, Point { x, y });
-        });
-
-        self.last_pasted_index += images.len() as i32;
-        self.num_images += images.len() as u32;
-    }
+    fn bulk_push(&mut self, images: &[&Image<P, image::ImageBuffer<P, Container>>]);
 }
---
+++ src/merger/known.rs
@@ -0,0 +1,257 @@
+use super::core::{Merger, Padding, Point};
+use crate::{cell::ImageCell, functions::paste, Image};
+
+use image::Pixel;
+use num_traits::Zero;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use std::ops::DerefMut;
+
+/// A known size merger that allows you to paste images onto a canvas. This merger is useful when you already know the size
+/// of all the images being pushed onto the canvas. This merger has multiple implementations, one for any container type and
+/// one for Vec specifically.
+///
+/// # Type Parameters
+/// * `P` - The pixel type of the underlying image.
+/// * `Container` - The underlying image buffer type. This must be dereferenceable to a slice of the underlying image's subpixels.
+pub struct KnownSizeMerger<P, Container>
+where
+    P: Pixel,
+    <P as Pixel>::Subpixel: Sync,
+    Container: DerefMut<Target = [P::Subpixel]> + Sync,
+{
+    canvas: ImageCell<P, image::ImageBuffer<P, Container>>,
+    image_dimensions: (u32, u32), // The dimensions of the images being pasted (images must be a uniform size)
+    num_images: u32,              // The number of images that have been pasted to the canvas
+    images_per_row: u32,          // The number of pages per row.
+    last_pasted_index: i32, // The index of the last pasted image, starts at -1 if not images have been pasted.
+    total_rows: u32,        // The total number of rows currently on the canvas.
+    padding: Option<Padding>,
+}
+
+impl<P, Container> KnownSizeMerger<P, Container>
+where
+    P: Pixel + Sync,
+    <P as Pixel>::Subpixel: Sync,
+    Container: DerefMut<Target = [P::Subpixel]> + Sync,
+{
+    /// Constructs a new KnownSizeMerger from a raw image buffer. This is useful if you need to use a specific container type that is not Vec. Typically,
+    /// you only need to use `new` to create a KnownSizeMerger unless you need to use a specific container type.
+    /// # Arguments
+    /// * `image_dimensions` - The dimensions of the images being pasted (images must be a uniform size)
+    /// * `images_per_row` - The number of images per row.
+    /// * `total_images` - The total number of images to be in the final canvas.
+    /// * `padding` - The padding between images, or None for no padding.
+    /// * `container` - The container to use for the underlying canvas. This container must be big enough to hold all the potential images
+    /// that will be pasted to the canvas.
+    ///
+    /// # Returns
+    /// * `Some` - If the merger was successfully created.
+    /// * `None` - If the merger could not be created. This will happen if the container is not large enough to fit all the images.
+    pub fn new_from_raw(
+        image_dimensions: (u32, u32),
+        images_per_row: u32,
+        total_images: u32,
+        padding: Option<Padding>,
+        container: Container,
+    ) -> Option<Self> {
+        let total_rows = (total_images + images_per_row - 1) / images_per_row;
+
+        let image_gaps_x = (images_per_row - 1) * padding.as_ref().map(|p| p.x).unwrap_or(0);
+        let image_gaps_y = (total_rows - 1) * padding.as_ref().map(|p| p.y).unwrap_or(0);
+
+        if let Some(canvas) = Image::new_from_raw(
+            (image_dimensions.0 * images_per_row) + image_gaps_x,
+            (image_dimensions.1 * total_rows) + image_gaps_y,
+            container,
+        ) {
+            Some(Self {
+                canvas: ImageCell::new(canvas),
+                image_dimensions,
+                num_images: 0,
+                images_per_row,
+                last_pasted_index: -1,
+                total_rows,
+                padding,
+            })
+        } else {
+            None
+        }
+    }
+
+    /// Returns the number of images that have been pasted to the canvas.
+    pub fn get_num_images(&self) -> u32 {
+        self.num_images
+    }
+
+    /// Returns the dimensions, (x, y), of the images being pasted to the canvas.
+    pub fn get_image_dimensions(&self) -> (u32, u32) {
+        self.image_dimensions
+    }
+
+    #[inline(always)]
+    fn additional_space(&self) -> u32 {
+        (self.images_per_row * self.total_rows) - self.num_images
+    }
+
+    fn get_paste_coordinates_unchecked(&self, index: u32) -> (u32, u32) {
+        let offset_x = index % self.images_per_row;
+        let offset_y = index / self.images_per_row;
+
+        let padding_x = self.padding.as_ref().map(|p| p.x).unwrap_or(0) * offset_x;
+        let padding_y = self.padding.as_ref().map(|p| p.y).unwrap_or(0) * offset_y;
+
+        let x = (offset_x * self.image_dimensions.0) + padding_x;
+        let y = (offset_y * self.image_dimensions.1) + padding_y;
+
+        (x, y)
+    }
+
+    fn get_next_paste_coordinates(&mut self) -> (u32, u32) {
+        if self.additional_space() == 0 {
+            panic!("No more space on the canvas!");
+        }
+
+        self.get_paste_coordinates_unchecked((self.last_pasted_index + 1) as u32)
+    }
+
+    /// Removes an image from the canvas at the given index. Indices start at 0 and work left to right, top to bottom. Most of the time
+    /// you will not need to use this function, and rather, can use the `remove_image` method instead. This method is useful if you need
+    /// to manually manage a specific Container type that is not `Vec`.
+    ///
+    /// # Arguments
+    /// * `index` - The index of the image to remove.
+    /// * `container` - The container to use to replace the image. The container must be the same size as the image being removed,
+    /// thus, the container must be the same size as the image dimensions.
+    ///
+    /// # Returns
+    /// * `Some` - If the image was successfully removed.
+    /// * `None` - If the image could not be removed. This will happen if the container is not large enough to fit the image.
+    fn remove_image_raw(&mut self, index: u32, container: Container) -> Option<()> {
+        let offset_x = index % self.images_per_row;
+        let offset_y = index / self.images_per_row;
+
+        let x = offset_x * self.image_dimensions.0;
+        let y = offset_y * self.image_dimensions.1;
+
+        let black_image =
+            Image::new_from_raw(self.image_dimensions.0, self.image_dimensions.1, container);
+
+        if let Some(black_image) = black_image {
+            paste(&self.canvas, &black_image, Point { x, y });
+            Some(())
+        } else {
+            None
+        }
+    }
+}
+
+impl<P> KnownSizeMerger<P, Vec<P::Subpixel>>
+where
+    P: Pixel + Sync,
+    <P as Pixel>::Subpixel: Sync,
+{
+    /// Constructs a new KnownSizeMerger. By default, this will create an underlying canvas with `Vec` as the container type. If you
+    /// need to use a different container type, you can use the `new_from_raw` method.
+    ///
+    /// # Arguments
+    /// * `image_dimensions` - The dimensions of the images being pasted (images must be a uniform size)
+    /// * `images_per_row` - The number of images per row.
+    /// * `total_images` - The total numbr of images to be in the final canvas.
+    /// * `padding` - The padding between images, or None for no padding.
+    pub fn new(
+        image_dimensions: (u32, u32),
+        images_per_row: u32,
+        total_images: u32,
+        padding: Option<Padding>,
+    ) -> Self {
+        let total_rows = (total_images + images_per_row - 1) / images_per_row;
+
+        let image_gaps_x = (images_per_row - 1) * padding.as_ref().map(|p| p.x).unwrap_or(0);
+        let image_gaps_y = (total_rows - 1) * padding.as_ref().map(|p| p.y).unwrap_or(0);
+
+        let canvas = Image::new(
+            (image_dimensions.0 * images_per_row) + image_gaps_x,
+            (image_dimensions.1 * total_rows) + image_gaps_y,
+        );
+
+        Self {
+            canvas: ImageCell::new(canvas),
+            image_dimensions,
+            num_images: 0,
+            images_per_row,
+            last_pasted_index: -1,
+            total_rows,
+            padding,
+        }
+    }
+
+    /// Removes an image from the canvas at a given index. Indexing starts at 0 and works left to right, top to bottom.
+    /// # Arguments
+    /// * `index` - The index of the image to remove.
+    pub fn remove_image(&mut self, index: u32) {
+        let container: Vec<<P as Pixel>::Subpixel> = vec![
+            Zero::zero();
+            (self.image_dimensions.0 * self.image_dimensions.1 * <P as Pixel>::CHANNEL_COUNT as u32)
+                as usize
+        ];
+
+        self.remove_image_raw(index, container).unwrap(); // Can always unwrap here because we know the buffer is the right size.
+
+        let offset_x = index % self.images_per_row;
+        let offset_y = index / self.images_per_row;
+
+        let x = offset_x * self.image_dimensions.0;
+        let y = offset_y * self.image_dimensions.1;
+
+        let black_image: Image<P, image::ImageBuffer<P, Vec<P::Subpixel>>> =
+            Image::new(self.image_dimensions.0, self.image_dimensions.1);
+
+        paste(&self.canvas, &black_image, Point { x, y });
+    }
+}
+
+impl<P, Container> Merger<P, Container> for KnownSizeMerger<P, Container>
+where
+    P: Pixel + Sync,
+    <P as Pixel>::Subpixel: Sync,
+    Container: DerefMut<Target = [P::Subpixel]> + Sync,
+{
+    fn get_canvas(&self) -> &Image<P, image::ImageBuffer<P, Container>> {
+        &self.canvas
+    }
+
+    fn into_canvas(self) -> Image<P, image::ImageBuffer<P, Container>> {
+        self.canvas.into_inner()
+    }
+
+    fn push(&mut self, image: &Image<P, image::ImageBuffer<P, Container>>) {
+        let (x, y) = self.get_next_paste_coordinates();
+
+        paste(&self.canvas, image, Point { x, y });
+
+        self.last_pasted_index += 1;
+        self.num_images += 1;
+    }
+
+    fn bulk_push(&mut self, images: &[&Image<P, image::ImageBuffer<P, Container>>]) {
+        // If we can't fit all the images we need to panic.
+        if self.additional_space() < images.len() as u32 {
+            // TODO: Maybe only take as many images as we can fit?
+            panic!("There is not enough space on the canvas to fit all the requested images.");
+        }
+
+        (0..images.len()).into_par_iter().for_each(|index| {
+            let image = images[index];
+
+            // The image coordinates can easily be calculated by using the last_pasted_index
+            // and making the calculations ourselves.
+            let offset_index = (index as i32 + self.last_pasted_index + 1) as u32;
+
+            let (x, y) = self.get_paste_coordinates_unchecked(offset_index);
+            paste(&self.canvas, image, Point { x, y });
+        });
+
+        self.last_pasted_index += images.len() as i32;
+        self.num_images += images.len() as u32;
+    }
+}
