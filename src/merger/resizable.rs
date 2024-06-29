431d20de83003fef576121de7724507e56ec7f68
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-06-29 00:24:08 -0400

    Add example for push resized image

--- src/merger/resizable.rs
+++ src/merger/resizable.rs
@@ -5,6 +5,18 @@ use image::Pixel;
 /// any existing merger to resize images before pasting. This is useful for when you have thousands of
 /// images to paste, but you don't want to hold all of them, at full size, in memory at once with a large
 /// canvas.
+/// # Type Parameters
+/// * `P` - The pixel type of the images to be resized.
+/// # Example
+/// ```
+/// use image_merger::{ResizableMerger, KnownSizeMerger, Merger, Rgb};
+///
+/// // Create a canvas that can hold 100x100 images, with 10 images per row, and 100 images total.
+/// let mut merger: KnownSizeMerger<Rgb<u8>, _> = KnownSizeMerger::new((100, 100), 10, 100, None);
+///
+/// // Push a 500x500 sized image onto the canvas. It will automatically be resized to 100x100.
+/// merger.push_resized(&image_merger::Image::new(500, 500));
+/// ```
 pub trait ResizableMerger<P>
 where
     P: Pixel + Sync,
