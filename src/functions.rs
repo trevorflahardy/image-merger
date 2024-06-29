7627d9de2bc15ce1624445ed984bdd1dfd79722f
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-06-28 23:40:43 -0400

    refactor: Add resize_nearest_neighbor function for image resizing

--- src/functions.rs
+++ src/functions.rs
@@ -1,6 +1,7 @@
 use crate::{cell::ImageCell, core::Image, merger::Point};
 use image::Pixel;
 use rayon::{
+    iter::IntoParallelIterator,
     prelude::{IndexedParallelIterator, ParallelIterator},
     slice::ParallelSlice,
 };
@@ -21,7 +22,7 @@ pub fn paste<P, Container>(
     <P as Pixel>::Subpixel: Sync,
     Container: DerefMut<Target = [P::Subpixel]>,
 {
-    // Go through each pixel in the image (at once), grab its relatve location on the canvas,
+    // Go through each pixel in the image (at once), grab its relative location on the canvas,
     // and alter the canvas underlying buffer to reflect the new pixel.
     let image_width = top.width();
     top.par_chunks_exact(<P as Pixel>::CHANNEL_COUNT as usize)
@@ -39,4 +40,64 @@ pub fn paste<P, Container>(
                 handout.unsafe_put_pixel(*pixel);
             }
         });
+}
+
+pub fn resize_nearest_neighbor<P, U>(
+    image: Image<P, U>,
+    nwidth: u32,
+    nheight: u32,
+) -> Image<P, image::ImageBuffer<P, Vec<<P as Pixel>::Subpixel>>>
+where
+    P: Pixel + Sync,
+    <P as Pixel>::Subpixel: Sync,
+    U: image::GenericImage<Pixel = P> + Sync,
+{
+    // Create a cell to hold the new image.
+    let new_image: Image<P, _> = Image::new(nwidth, nheight);
+    let cell = ImageCell::new(new_image);
+
+    // Grab the ratios of the new image to the old image.
+    let height_ratio = image.height() as f32 / nheight as f32;
+    let width_ratio = image.width() as f32 / nwidth as f32;
+
+    (0..nwidth).into_par_iter().for_each(|i| {
+        (0..nheight).into_par_iter().for_each(|j| {
+            let x = (i as f32 * width_ratio) as u32;
+            let y = (j as f32 * height_ratio) as u32;
+
+            let pixel = image.get_pixel(x, y);
+
+            unsafe {
+                let mut handout = cell.request_handout(i, j);
+                handout.unsafe_put_pixel(pixel)
+            }
+        })
+    });
+
+    cell.into_inner()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use image::{ImageBuffer, Rgb};
+
+    #[test]
+    fn test_resize_nearest_neighbor() {
+        let mut image = ImageBuffer::new(100, 100);
+        for i in 0..100 {
+            for j in 0..100 {
+                image.put_pixel(i, j, Rgb([255, 0, 0]));
+            }
+        }
+
+        let image = Image::from(image);
+        let new_image = resize_nearest_neighbor(image, 10, 10);
+
+        for i in 0..10 {
+            for j in 0..10 {
+                assert_eq!(*new_image.get_pixel(i, j), Rgb([255, 0, 0]));
+            }
+        }
+    }
 }
